2a93ef75bef1e6d26d57fe4ccb6f4c4a929a4b7c
Maciej Aleksandrowicz <macvek@gmail.com>
2024-01-16 12:52:05 +0100

    Removed old logic from main.cxx, leaving it only as placeholder

--- main.cxx
+++ main.cxx
@@ -1,53 +1,6 @@
-#include "console.h"
-#include "timer.h"
-#include "log.h"
-#include "input.h"
-#include "output.h"
-#include <stdio.h>
+#include <iostream>
 using namespace std;
 
-struct MyOnTick : public TimerOnTick {
-    void onTick() {
-        std::cout << "On My Tick" << ENDLINE;
-    }
-};
-
 int main(int argc, char** argv) {
-    Console console;
-    MyOnTick myOnTick;
-    Input input;
-    try {
-        Timer timer(200, &myOnTick);
-        console.enableRaw();
-
-        for (;;) {
-            input.waitFor();
-            Key what = input.getKey();
-            if (what.type != STANDARD) {
-                continue;
-            }
-
-            if (what.value == 'x') {
-                cout << "I'm done" << ENDLINE;
-                break;
-            } 
-            else if (what.value == 's') {
-                timer.stop();
-            } 
-            else if (what.value == 'r') {
-                timer.start();
-            } 
-            else if (what.value == 'E') {
-                Log::error() << "Call me inside a method" << ENDLINE;
-                Log::panicOnError();
-                cout << "No error" << ENDLINE;
-            } 
-            else {
-                cout << "not x , try again" << ENDLINE;
-            }
-        }
-    }
-    catch (LogPanic& panic) {
-
-    }
+    cout << "So far empty project " << endl;
 }
