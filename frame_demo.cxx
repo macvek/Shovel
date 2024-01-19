f7e427a05abda4bcb2806ede083fe03835c4d981
Maciej Aleksandrowicz <macvek@gmail.com>
2024-01-19 18:02:38 +0100

    Added drawing utf8 frame

--- frame_demo.cxx
+++ frame_demo.cxx
@@ -19,10 +19,21 @@ int main(int argc, char** argv) {
         "@x                   x@",
     0,0);
 
+    std::string MappedWindow = "\x81\x82\x83\x84 \x84\x85\x82\x86";
+    
     f.drawFrame(b, 1, 1, 21, 5,     Frame::Hash);
     f.drawFrame(b, 2, 2, 4, 4,      Frame::DashAndSlash);
+    f.drawFrame(b, 5, 2, 7, 4,      MappedWindow);
 
-    RenderBuffer scene(80,24,'.',true);
+    SpecialCharsMap utf8Frame;
+    utf8Frame[0x81] = "┌";
+    utf8Frame[0x82] = "─";
+    utf8Frame[0x83] = "┐";
+    utf8Frame[0x84] = "│";
+    utf8Frame[0x85] = "└";
+    utf8Frame[0x86] = "┘";
+    
+    RenderBuffer scene(80,24,'.',true, utf8Frame);
 
     t.clear();
   
