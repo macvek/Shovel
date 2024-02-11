0b8751ea208fd192befa33e5e0bfb347a7162bed
Maciej Aleksandrowicz <macvek@gmail.com>
2024-02-11 15:39:59 +0100

    Verifies that event is executed only if found.

--- events_test.cxx
+++ events_test.cxx
@@ -5,17 +5,27 @@ using namespace std;
 
 struct Handler {
     virtual void onEvent() {}
+    virtual void onConsumeInt(int value) {}
 };
 
 struct MyHandler : public Handler {
     void onEvent() {
         cout << "Got Event!" << endl;
     }
+
+    void onConsumeInt(int it) {
+        cout << "On consume event " << it <<  endl;
+    }
 };
 
 struct TriggerMyEvent : public EventTrigger<Handler> {
     void trigger(const std::string& eventName, Handler* handler) {
-        handler->onEvent();
+        if ("MyEvent" == eventName) {
+            handler->onEvent();
+        }
+        if ("ConsumeInt" == eventName) {
+            handler->onConsumeInt(123);
+        }
     }
 };
 
@@ -25,7 +35,11 @@ int main() {
     TriggerMyEvent triggerMyEvent;
     
     e.registerEvent("MyEvent", &h);
+    e.registerEvent("MyEvent", &h);
+    e.registerEvent("ConsumeInt", &h);
     e.triggerEvent("MyEvent", triggerMyEvent);
+    e.triggerEvent("ConsumeInt", triggerMyEvent);
+    e.triggerEvent("UnknownEvent", triggerMyEvent);
 
     return 0;
 }
