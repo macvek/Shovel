209ef0a7031f7e4842f9475311014a0c27d8e7cc
Maciej Aleksandrowicz <macvek@gmail.com>
2024-02-11 15:48:48 +0100

    Added unregistering and cleanup of empty container

--- events.h
+++ events.h
@@ -13,12 +13,27 @@ template<typename T> class Events {
 
     public:
     void registerEvent(std::string eventName, T* handler);
+    void unregisterEvent(std::string eventName, T* handler);
     void triggerEvent(std::string eventName, EventTrigger<T> &trigger);
 };
 
 template<typename T> void Events<T>::registerEvent(std::string eventName, T* aHandler) {
     this->hub[eventName].insert(aHandler);
 }
+
+template<typename T> void Events<T>::unregisterEvent(std::string eventName, T* aHandler) {
+    if (this->hub.find(eventName) == this->hub.end()) {
+        return;
+    }
+    
+    this->hub[eventName].erase(aHandler);
+
+    if (this->hub[eventName].empty()) {
+        this->hub.erase(eventName);
+    }
+    
+}
+
 
 template<typename T> void Events<T>::triggerEvent(std::string eventName, EventTrigger<T> &callToTrigger) {
     if (this->hub.find(eventName) == this->hub.end()) {
--- events_test.cxx
+++ events_test.cxx
@@ -18,13 +18,15 @@ struct MyHandler : public Handler {
     }
 };
 
+static int counter = 0;
+
 struct TriggerMyEvent : public EventTrigger<Handler> {
     void trigger(const std::string& eventName, Handler* handler) {
         if ("MyEvent" == eventName) {
             handler->onEvent();
         }
         if ("ConsumeInt" == eventName) {
-            handler->onConsumeInt(123);
+            handler->onConsumeInt(++counter);
         }
     }
 };
@@ -37,9 +39,30 @@ int main() {
     e.registerEvent("MyEvent", &h);
     e.registerEvent("MyEvent", &h);
     e.registerEvent("ConsumeInt", &h);
+
     e.triggerEvent("MyEvent", triggerMyEvent);
     e.triggerEvent("ConsumeInt", triggerMyEvent);
     e.triggerEvent("UnknownEvent", triggerMyEvent);
+
+    if (counter != 1) {
+        cerr << "Failed, counter should be 1";
+        exit(1);
+    }
+    e.unregisterEvent("ConsumeInt", &h);
+    e.triggerEvent("ConsumeInt", triggerMyEvent);
+
+    if (counter != 1) {
+        cerr << "Failed, counter should be 1, as event was unregistered";
+        exit(1);
+    }
+
+    e.registerEvent("ConsumeInt", &h);
+    e.triggerEvent("ConsumeInt", triggerMyEvent);
+
+    if (counter != 2) {
+        cerr << "Failed, counter should be 2, got: " << counter<< ". Event was registered again and retriggered";
+        exit(1);
+    }
 
     return 0;
 }
