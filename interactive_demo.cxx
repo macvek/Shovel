313bfc3a5b90951607db9908ec7bb93cd38c5aae
Maciej Aleksandrowicz <macvek@gmail.com>
2024-02-21 20:38:31 +0100

    Added menu

--- interactive_demo.cxx
+++ interactive_demo.cxx
@@ -2,6 +2,10 @@
 #include "terminal.h"
 #include "input.h"
 #include "events.h"
+#include "renderbuffer.h"
+#include "frame.h"
+#include <cstdlib>
+#include <sstream>
 
 struct OnKeyHandler {
     virtual void onKey(Key &k) {}
@@ -13,19 +17,56 @@ Input input;
 Events<OnKeyHandler, Key> onKeyEvents;
 bool stopApplication = false;
 
+SpecialCharsMap base = SpecialCharsMapFactory::create();
+RenderBuffer blankScene(80,24,' ',true, base);
+RenderBuffer backBuffer = blankScene;
+RenderBuffer frontBuffer = blankScene;
+Frame f;
+
+int selectedItem = 0;
+
+void drawMenu() {
+    int posX = 30;
+    int posY = 1;
+    int width = 20;
+    int height = 10;
+    f.drawFrame(frontBuffer,posX,posY,posX+width+1,posY+height+1, Frame::SingleBorder);
+    
+    for (int i=0;i<10;i++) {
+        std::stringstream caption;
+        caption << "Menu item #" << i+1;
+        frontBuffer.writeText(caption.str(), posX+1, posY+1+i);
+        if ( i == selectedItem ) {
+            frontBuffer.writeColorLine(posX + 1, posY + 1 + i, 20, Terminal::MakeColor(Terminal::BLACK, Terminal::WHITE));
+        }
+    }
+}
+
+void render() {
+    frontBuffer.copyFrom(blankScene);
+    drawMenu();
+    frontBuffer.toTerminal(terminal, 1, 1, true);
+    terminal.flush();
+}
+
+
 struct KeyHandlerCapture : public OnKeyHandler {
     void onKey(Key &k) {
         if (k.type == KeyType::ESCAPE) {
             stopApplication = true;
+            return;
         }
-        else {
-            std::cout << ".";
-            std::cout.flush();
+        
+        if (k.type == KeyType::ARROW_DOWN) {
+            ++selectedItem;
         }
+        else if (k.type == KeyType::ARROW_UP) {
+            --selectedItem;
+        }
+        
+        render();
     }
 } mainKeyCapture;
-
-
 
 
 struct TriggerMyEvent : public EventTrigger<OnKeyHandler, Key> {
@@ -35,13 +76,7 @@ struct TriggerMyEvent : public EventTrigger<OnKeyHandler, Key> {
     }
 } triggerKeyEvent;
 
-
-int main(int argc, char** argv) {
-    console.enableRaw();
-    terminal.showCursor(false);
-    terminal.clear();
-    
-    onKeyEvents.registerEvent("Key", &mainKeyCapture);
+void mainLoop() {
     for (;;) {
         input.waitFor();
         Key k = input.getKey();
@@ -51,6 +86,17 @@ int main(int argc, char** argv) {
             break;
         }
     }
+}
+
+int main(int argc, char** argv) {
+    console.enableRaw();
+    terminal.showCursor(false);
+    terminal.clear();
+    terminal.flush();
+    
+    onKeyEvents.registerEvent("Key", &mainKeyCapture);
+
+    mainLoop();
 
     console.restore();
     terminal.showCursor(true);
